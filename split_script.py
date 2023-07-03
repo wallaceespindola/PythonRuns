346b7deeb67714accdea2f2d2514c7b2f5ca3f6d
Wallace Espindola <wallace.espindola@gmail.com>
2023-07-03 17:12:20 +0200

    Update split_script.py to log steps in script
    Added log file and console logging

--- split_script.py
+++ split_script.py
@@ -1,15 +1,34 @@
 import argparse
+import logging
 
-# split a text input file (like a log) into a given number of chunks (ex: 1 big file split into 10 small ones)
+# split a text input file (like a log) into a given number of chunks.
+# Example: 1 big file split into 10 small ones.
+# Use: python split_script.py path_to_your_log_file.log 10
 def split_file(file_path, num_chunks):
+    # Set up logging to file and console
+    logging.basicConfig(filename='split_file_log.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+    logging.info(f'Starting to split file: {file_path} into {num_chunks} chunks.')
+
     with open(file_path, 'r') as file:
         lines = file.readlines()
         chunk_size = len(lines) // num_chunks
         for i in range(num_chunks):
             start = i * chunk_size
             end = start + chunk_size if i < num_chunks - 1 else None
-            with open(f"{file_path}_part_{i + 1}.log", 'w') as output_file:
+            output_file_name = f"{file_path}_part_{i + 1}.log"
+            with open(output_file_name, 'w') as output_file:
                 output_file.writelines(lines[start:end])
+
+            # Log the output file name
+            logging.info(f'Split file created: {output_file_name}')
+
+    logging.info(f'Finished splitting file: {file_path}')
 
 # Parsing command line arguments
 parser = argparse.ArgumentParser(description='Split a large log file into smaller chunks.')
@@ -18,5 +37,5 @@ parser.add_argument('num_chunks', type=int, help='Number of chunks to split the 
 
 args = parser.parse_args()
 
-# Example usage:
+# Call the split_file function
 split_file(args.file_path, args.num_chunks)
