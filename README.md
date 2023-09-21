ae79f465df7e2e077dda581b50f6d0cd12a95777
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2023-09-21 15:27:48 +0200

    Adding new data analysis test runs

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# PythonRuns
+![Python](https://www.python.org/static/community_logos/python-logo-generic.svg)
 
+# PythonRuns
 Tests on Python Code Examples.
 
 ## Examples
-
 * [calculate_power.py](sources%2Fcalculate_power.py): calculate a power x^y of a number.
 * [camel_2_snake_case.py](sources%2Fcamel_2_snake_case.py): converts a file text content from camel case to snake case.
 * [convert_pdf_to_docx.py](sources%2Fconvert_pdf_to_docx.py): converts a pdf file to a docx file.
@@ -14,7 +14,10 @@ Tests on Python Code Examples.
 * [split_script.py](sources%2Fsplit_script.py): splits a text input file (like a log) into a given number of chunks.
 * [fake_data.py](sources%2Ffake_data.py): creates fake test data using Faker.
 * [qrcode.py](sources%2Fqrcode.py): creates a QR-Code from your data.
+* [fines_ibama_brazil.py](sources%2Ffines_ibama_brazil.py): Enviromental fines research in Brazil government site.
 
 ### Author
+* Wallace Espindola, Sr. Software Engineer / Java & Python Dev (wallace.espindola@gmail.com)
 
-* Wallace Espindola (wallace.espindola@gmail.com)
+### License
+**Free Software, Apache 2.0**
---
+++ sources/fines_ibama_brazil.py
@@ -0,0 +1,106 @@
+# REFERENCE URL: 'http://dadosabertos.ibama.gov.br/dados/SICAFI/AC/Quantidade/multasDistribuidasBensTutelados.json'
+
+import locale
+import pandas as pd
+import requests
+import urllib3
+
+# Disable SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Locale PT BR - R$
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+
+def get_states():
+    return [
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    ]
+
+
+def main():
+    print("IBAMA Brazil - Accessing environmental fines list...")
+
+    print("\n>>> OPTIONS:")
+    for code, name in get_states():
+        print(f'{code} - {name}')
+
+    while True:
+        typed_state = input("\n>>> ENTER YOUR OPTION: ").upper()
+        if any(code == typed_state for code, name in get_states()):
+            state = typed_state
+            state_name = next(name for code, name in get_states() if code == typed_state)
+            break
+        else:
+            print("Invalid option. Please type a valid state acronym.")
+
+    print(f"\n### Fetching data for: {state} - {state_name}...")
+
+    response = requests.get(
+        f'http://dadosabertos.ibama.gov.br/dados/SICAFI/{state}/Quantidade/multasDistribuidasBensTutelados.json',
+        verify=False # No SSL verification
+    )
+
+    if response.status_code == 200:
+        list_of_process = response.json()
+        amount = len(list_of_process['data'])
+        print(f"\n>>> {amount} processes found")
+        categories = ["Fauna", "Flora", "Pesca", "Controle ambiental", "Outras"]
+
+        for category in categories:
+            print(f"\nAccessing fines about {category} ...")
+            filtered_processes = [process for process in list_of_process['data'] if process["tipoInfracao"] == category]
+
+            # Uncomment to see the data
+            #print(f'\nProcesses for category {category}: \n{filtered_processes}\n')
+
+            count = len(filtered_processes)
+            print(f"{count} fines related to {category} found!")
+
+            row = {
+                'municipio': [process["municipio"] for process in filtered_processes],
+                'nomeRazaoSocial': [process["nomeRazaoSocial"] for process in filtered_processes],
+                'valorAuto': [locale.currency(process["valorAuto"], grouping=True, symbol=True) for process in filtered_processes],
+                'dataAuto': [process["dataAuto"] for process in filtered_processes],
+                'situacaoDebito': [process["situacaoDebito"] for process in filtered_processes],
+                'enquadramentoLegal': [process["enquadramentoLegal"] for process in filtered_processes],
+            }
+
+            df = pd.DataFrame(row)
+            output_file = f"../output/{category}.csv"
+            df.to_csv(output_file, index=True, index_label='ID')
+            print(f'Fines related to {category} have been saved to CSV file: [{output_file}].')
+    else:
+        print(f"REQUEST ERROR - RESPONSE CODE: [{response.status_code}]")
+
+    print("\nIBAMA Brazil - search completed.")
+
+
+if __name__ == '__main__':
+    main()
