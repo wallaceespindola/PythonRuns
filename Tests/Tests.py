1bd1d505c18f82701b1aa0c5bdcb792ab637f8ce
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2024-02-13 17:52:38 +0100

    Adding extra tests for checking path separators

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -7,10 +7,20 @@ class Tests(unittest.TestCase):
         """
         Test valid for windows, mac or linux, gets the default separator for the current OS ('/' or '\')
         """
-        print(os.sep)  # Outputs the path separator character
-        print(os.path.sep)  # Outputs the same path separator character
+        print("\n1) os.sep: " + os.sep)  # Outputs the path separator character
+        print(os.sep + 'test1')
+        print(str("test2").join(os.sep))  # do not join the string, ignores test2
+        print(os.sep.join('test3'))
+
+        print("\n2) os.path.sep: " + os.path.sep)  # Outputs the same path separator character
+        print(os.path.sep + 'test4')
+        print(os.path.sep.join('test5'))
+
+        print("\n3) os.path.join: ")  # Outputs the same path separator character
+        print(os.path.join('test6', 'test7', 'test8'))
 
         # Check if they are the same
+        print("\n4) os.sep == os.path.sep:")
         print(os.sep == os.path.sep)  # Outputs True
 
         self.assertEqual(os.sep, os.path.sep)
