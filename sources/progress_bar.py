8a3da2a846983a6deac1178b5c495afb403d3413
Wallace Espindola <wallace.espindola@gmail.com>
2024-07-11 16:33:24 +0200

    Tests for progress bar on cli

---
+++ sources/progress_bar.py
@@ -0,0 +1,24 @@
+import time
+
+from progress.bar import Bar
+from tqdm import tqdm
+
+
+def long_process_bar():
+    print("Progress bar with progress")
+    bar = Bar('Processing', max=50)
+    for _ in range(50):
+        time.sleep(0.1)  # Simulate work being done
+        bar.next()
+    bar.finish()
+
+
+def long_process_tqdm():
+    print("Progress bar with tqdm")
+    for _ in tqdm(range(50), desc="Processing"):
+        time.sleep(0.1)  # Simulate work being done
+
+
+if __name__ == "__main__":
+    long_process_tqdm()
+    long_process_bar()
