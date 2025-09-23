c7d88fb372c534b562e036ac810c2461ed6ed960
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2025-09-23 12:48:51 +0200

    modif script

--- scripts/daily_git_commit.sh
+++ scripts/daily_git_commit.sh
@@ -11,7 +11,7 @@ case "$(date +%u)" in
   2) REPO="$HOME/git/PythonRuns" ;;                         # Tue
   3) REPO="$HOME/git/flashcards-app" ;;                     # Wed
   4) REPO="$HOME/git/investment-portfolio-python" ;;        # Thu
-  5) REPO="$HOME/git/workflow-kafka-process-file-output" ;; # Fri
+  5) REPO="$HOME/git/pydantic-tests" ;;                     # Fri
   6) REPO="$HOME/git/spring-kafka-batch-demo" ;;            # Sat
   7) REPO="$HOME/git/python-flask-app" ;;                   # Sun
   *) echo "Unknown weekday"; exit 2 ;;
