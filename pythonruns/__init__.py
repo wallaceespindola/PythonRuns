a372005c311c86a25d352233380211312d7ff1fd
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2025-10-03 12:16:28 +0200

    add root package for module

---
+++ pythonruns/__init__.py
@@ -0,0 +1,15 @@
+"""
+PythonRuns - Python tests and proofs of concept.
+
+A collection of Python examples, utilities, and learning projects.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Wallace Espindola"
+__email__ = "wallace.espindola@gmail.com"
+
+# Import main modules for easy access
+from .hello_world import say_hello
+
+__all__ = ["say_hello"]
+
